6b6ac2729b858da72573271bcf845ecd1b936617
Sai Corp <74317023+SaiCorporation@users.noreply.github.com>
2022-02-18 12:17:17 -0500

    Add files via upload

---
+++ Make your own google url.py
@@ -0,0 +1,6 @@
+print('Hi.')
+input("This python file will make you your very own google homepage url. Please write OK to continue")
+backhalf=input("Please write the backhalf")
+
+print(' Your url is: https://www.google.com/?', backhalf)
+input('')
